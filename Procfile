0a9401b34b3c20191243d82a34a6eb0e1bc2e6d9
Yusuke Watase <ywatase@gmail.com>
2016-04-08 11:34:51 +0900

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:linebot
+web: linebot
--- web-api.go
+++ web-api.go
@@ -5,33 +5,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
 
 /* --- JSON define */
-type ApiLocation struct {}
-type ApiContentMetadata struct {}
+type ApiLocation struct{}
+type ApiContentMetadata struct{}
 
 type ApiContent struct {
-	Id	string	`json:id`
-	ContentType	int	`json:contentType`
-	From	string	`json:from`
-	CreatedTime	int	`json:createdTime`
-	To []string `json:to`
-	ToType int `json:toType`
+	Id              string             `json:id`
+	ContentType     int                `json:contentType`
+	From            string             `json:from`
+	CreatedTime     int                `json:createdTime`
+	To              []string           `json:to`
+	ToType          int                `json:toType`
 	ContentMetadata ApiContentMetadata `json:contentMetadata`
-	Text string `json:text`
-	Location ApiLocation `json:location`
+	Text            string             `json:text`
+	Location        ApiLocation        `json:location`
 }
 type ApiResult struct {
-	From string `json:from`
-	FromChannel string `json:fromCannel`
-	To []string `json:to`
-	ToChannel int `json:toChannel`
-	EventType string `json:eventType`
-	Id string `json:id`
-	Content ApiContent `json:content`
+	From        string     `json:from`
+	FromChannel string     `json:fromCannel`
+	To          []string   `json:to`
+	ToChannel   int        `json:toChannel`
+	EventType   string     `json:eventType`
+	Id          string     `json:id`
+	Content     ApiContent `json:content`
 }
 type MessageRecieve struct {
 	Result []ApiResult `json:result`
@@ -44,7 +45,7 @@ type ApiResponse struct {
 /* --- controller          */
 /* ----------------------- */
 func apiRequest(w http.ResponseWriter, r *http.Request) {
-	textContent := ApiContent{ContentType: 1,ToType: 1, Text: "hello"}
+	textContent := ApiContent{ContentType: 1, ToType: 1, Text: "hello"}
 	result := ApiResult{ToChannel: 1441301333, EventType: "138311609100106403", Content: textContent}
 	ret := ApiResponse{Result: []ApiResult{result}}
 	request := ""
@@ -61,11 +62,11 @@ func apiRequest(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	// type check
-//     if r.Method != "POST" {
-//         ret.Status = 1
-//         ret.Code = "Not POST method"
-//         return
-//     }
+	//     if r.Method != "POST" {
+	//         ret.Status = 1
+	//         ret.Code = "Not POST method"
+	//         return
+	//     }
 
 	// request body
 	rb := bufio.NewReader(r.Body)
@@ -81,19 +82,19 @@ func apiRequest(w http.ResponseWriter, r *http.Request) {
 	var msg MessageRecieve
 	b := []byte(request)
 	err := json.Unmarshal(b, &msg)
-     if err != nil {
-         result.Content = ApiContent{ContentType: 1,ToType: 1, Text: "JSON parse error." }
-         return
-	 }
+	if err != nil {
+		result.Content = ApiContent{ContentType: 1, ToType: 1, Text: "JSON parse error."}
+		return
+	}
 	result.To = []string{msg.Result[0].Content.From}
 
 	// mecab parse
-//     result, err := mecab.Parse(dec.Sentence)
-//     if err == nil {
-//         for _, n := range result {
-//             ret.Result = append(ret.Result, n)
-//         }
-//     }
+	//     result, err := mecab.Parse(dec.Sentence)
+	//     if err == nil {
+	//         for _, n := range result {
+	//             ret.Result = append(ret.Result, n)
+	//         }
+	//     }
 
 }
 
@@ -107,8 +108,11 @@ func hello(res http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/callback", apiRequest)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		panic(err)
 	}
